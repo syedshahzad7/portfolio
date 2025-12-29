ce3c74db26435f8f80ac8065f022669d661a073e
Syed Shahzad Abdul Wajid <shahzadksa777@gmail.com>
2025-12-29 16:15:59 -0500

    Updated lottie

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -42,22 +42,21 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          {/* Subheadline */}
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            Software Development | AI/ML Enthusiast
           </p>
 
-          {/**
-           *  Link: https://ui.aceternity.com/components/text-generate-effect
-           *
-           *  change md:text-6xl, add more responsive code
-           */}
+          {/* Headline */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Transforming Ideas into Smart, Scalable Applications"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
+          {/* Intro */}
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Shahzad, a Next.js Developer based in the USA.
+            Hi! I&apos;m Shahzad, a US-based Software Developer passionate about
+            building AI-powered applications.
           </p>
 
           <a href="#about">
